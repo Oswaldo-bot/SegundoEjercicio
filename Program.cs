68484116badfe2a1dbcdd90927beb4b53cf774f9
Oswaldo Duran Chaverra <sombrita98@hotmail.com>
2021-02-04 08:40:36 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SegundoEjercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                float valorhr, hrstrabMES, antigue;
+                string nombre;
+
+                Console.Write("Nombre:");
+                nombre = Console.ReadLine();
+
+                Console.Write("Valor hora:");
+                valorhr = float.Parse(Console.ReadLine());
+
+                Console.Write("Antiguedad:");
+                antigue = float.Parse(Console.ReadLine());
+
+                Console.Write("Horas trabajadas:");
+                hrstrabMES = float.Parse(Console.ReadLine());
+
+                Console.Clear();
+                Console.WriteLine("Nombre: {0}", nombre);
+                Console.WriteLine("Antiguedad: {0}", antigue);
+                Console.WriteLine("Valor hora: {0}", valorhr);
+                Console.WriteLine("Total a cobrar: {0}", valorhr * hrstrabMES + antigue * 50000);// TOTAL
+                Console.WriteLine("Total del descuento: {0}", (valorhr * hrstrabMES + antigue * 50000) * 0.15);// TOTAL * 0,15
+                Console.WriteLine("Total con descuento: {0}", ((valorhr * hrstrabMES + antigue * 50000) - (valorhr * hrstrabMES + antigue * 30) * 0.15));// TOTAL - DESC
+                Console.Read();
+            }
+        }
+    }
+}
+
